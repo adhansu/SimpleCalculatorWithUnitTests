371ef009e4e3117916a6becffb5308be1b247cc2
adhansu <86222376+adhansu@users.noreply.github.com>
2021-09-19 12:25:21 +0300

    First code commit

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+import math
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        return a / b
+
+    @staticmethod
+    def squareroot(a):
+        return math.sqrt(a)
+
